0a917910be8985ce8b1dfdec642c725225a9d99e
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2015-03-25 00:12:58 +0530

    Got cyclops eye beam correctly in Test1.unity

---
+++ Assets/Scripts/CyclopsScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CyclopsScript : MonoBehaviour {
+
+	LineRenderer line;
+
+	// Use this for initialization
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = false;
+		line.material = new Material (Shader.Find ("Particles/Additive"));
+		line.SetColors (Color.red, Color.red);
+		line.SetWidth ((float)0.15, (float)0.15);
+
+		//Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("left ctrl")) {
+			StopCoroutine("FireLaser");
+			StartCoroutine("FireLaser");
+		}
+	}
+
+	IEnumerator FireLaser(){
+		line.enabled = true;
+
+		while (Input.GetKey("left ctrl")) {
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+
+			line.SetPosition(0, ray.origin);
+
+			if (Physics.Raycast(ray, out hit, 100))
+				line.SetPosition(1, hit.point);
+			else
+				line.SetPosition(1, ray.GetPoint(100));
+
+			yield return null;
+		}
+
+		line.enabled = false;
+	}
+}
---
+++ Assets/Scripts/MultiBeamScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiBeamScript : MonoBehaviour {
+
+	GameObject newBeamL, newBeamR;
+	float i;
+
+	// Use this for initialization
+	void Start () {
+		for (i = (float)0.5; i>0; i-=(float)0.02) {
+			//Debug.Log (i);
+			newBeamL = new GameObject();
+			newBeamL.transform.parent = transform;
+			newBeamL.AddComponent<LineRenderer>();
+			newBeamL.AddComponent<CyclopsScript>();
+			newBeamL.transform.localPosition = new Vector3(-i, 0, (float)0.5);
+			newBeamL.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+
+			newBeamR = new GameObject();
+			newBeamR.transform.parent = transform;
+			newBeamR.AddComponent<LineRenderer>();
+			newBeamR.AddComponent<CyclopsScript>();
+			newBeamR.transform.localPosition = new Vector3(i, 0, (float)0.5);
+			newBeamR.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
